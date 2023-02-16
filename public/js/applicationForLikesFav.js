9dd7a837fb6f1f03038cd7cf69e5bcbaa675a652
Alisa Savorskaya <alyssen95@mail.ru>
2023-02-16 18:57:04 +0300

    likes for one card

---
+++ public/js/applicationForLikesFav.js
@@ -0,0 +1,9 @@
+const manyLikeBtns = document.querySelectorAll('.many-like-btns');
+
+if (manyLikeBtns) {
+  manyLikeBtns.forEach((btn) =>
+    btn.addEventListener('click', (event) => {
+      console.log(123);
+    })
+  );
+}
